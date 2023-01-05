527db298ae5f0a7e26b1c5b9f5f258c71524908a
aashi gupta <aashigupta165@gmail.com>
2023-01-05 19:06:19 +0530

    Create table book in MYSQL & connect by using JDBC.

--- src/main/java/com/book/servlet/RegisterServlet.java
+++ src/main/java/com/book/servlet/RegisterServlet.java
@@ -15,12 +15,12 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private static final String query = "INSERT INTO book(NAME, EDITION, PRICE) VALUES(?,?,?)";
 
 	@Override
@@ -61,7 +61,7 @@ public class RegisterServlet extends HttpServlet {
 			printWriter.println("<h1>"+e.getMessage()+"</h1>");
 		}
 	}
-	
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doGet(req,resp);
