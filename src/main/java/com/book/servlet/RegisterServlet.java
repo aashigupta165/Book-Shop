a670b794f485b0b43ea08c8bbebecc6f32b84c15
aashi gupta <aashigupta165@gmail.com>
2023-10-22 23:56:36 +0530

    Book Registration Servlet setup.

---
+++ src/main/java/com/book/servlet/RegisterServlet.java
@@ -0,0 +1,31 @@
+package com.book.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class RegisterServlet extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// get PrintWriter
+		PrintWriter printWriter = resp.getWriter();
+		// set content type
+		resp.setContentType("text/html");
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req,resp);
+	}
+}
